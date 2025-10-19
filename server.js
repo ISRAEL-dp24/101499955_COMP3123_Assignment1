a9083d650b56e1c8345e1d221b1cfb5b5454dd4e
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2025-10-19 09:23:37 -0400

    Adding Docker Deployment

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const app = express();
 app.use(express.json());
 const port = 3000;
 
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/comp3123_assignment1')
+mongoose.connect('mongodb://mongo:27017/comp3123_assignment1', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.log('MongoDB connection error:', err));
 
